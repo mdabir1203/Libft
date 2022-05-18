195ef8d75edc1fc60f29a2dabca6c11e01b8547a
Mohammad abir Abbas <mabbas@c2s13d70.42wolfsburg.de>
2022-05-18 16:46:19 +0200

    Have 2 memory leaks

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,19 +6,36 @@
 /*   By: mabbas <mabbas@student.wolfsburg42.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 01:59:20 by mabbas            #+#    #+#             */
-/*   Updated: 2022/05/17 17:23:51 by mabbas           ###   ########.fr       */
+/*   Updated: 2022/05/18 16:37:18 by mabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include<stdio.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*substr;
+	char	*newstr;
 
-	substr = malloc(3 * s);
-	if (!substr)
+	if (s == NULL)
 		return (NULL);
-	substr = (char *) ft_strnstr (s, start, len);
-	return (substr);
+	if (ft_strlen(s) < (int)len)
+		len = ft_strlen(s);
+	newstr = malloc(sizeof (char) * (len + 1));
+	//newstr = ft_strdup(s);
+	if (!(newstr))
+		return (NULL);
+	if ((int) start >= ft_strlen(s))
+		return (ft_strdup(""));
+	newstr = ft_memcpy(newstr, s + start, len);
+	newstr[len] = '\0';
+	return (newstr);
 }
+
+/* int main(int argc, char *argv[])
+{
+	char *sad;
+	sad = "01231231232131245";
+	printf("The copied substring:%s", ft_substr(sad,0,100));
+
+} */

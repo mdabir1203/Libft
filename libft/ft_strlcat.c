3719a49aaf2e7a16f9f862fe80970b157bc39278
Mohammad abir Abbas <mabbas@c2s13d70.42wolfsburg.de>
2022-05-21 23:20:44 +0200

    put good var names

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mabbas <mabbas@student.wolfsburg42.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 17:02:37 by mabbas            #+#    #+#             */
-/*   Updated: 2022/05/13 16:56:14 by mabbas           ###   ########.fr       */
+/*   Updated: 2022/05/21 23:20:10 by mabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,33 @@
  * Always NULL terminates (unless size <= strlen(dst)).
  * Returns strlen(src) + MIN(siz, strlen(initial dst)).
  * If retval >= siz, truncation occurred.
- *
+ * tmpdest++ mean increase the value tmpdst = tmpdst + 1
  */
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t len)
 {
-	char		*d;
-	const char	*s;
+	char		*tmpdest;
+	const char	*tmpsrc;
 	size_t		d_len_empty;
 	size_t		d_len;
 
-	d = dst;
-	s = (char *) src;
+	tmpdest = dst;
+	tmpsrc = (char *) src;
 	d_len_empty = len;
 	d_len = 0;
-	while ((d_len_empty-- != 0) && (*d != '\0'))
-		d++;
-	d_len = d - dst;
+	while ((d_len_empty-- != 0) && (*tmpdest != '\0'))
+		tmpdest++;
+	d_len = tmpdest - dst;
 	d_len_empty = len - d_len;
 	if (d_len_empty == 0)
-		return (d_len + ft_strlen(s));
-	while (*s != '\0' )
+		return (d_len + ft_strlen(tmpsrc));
+	while (*tmpsrc != '\0' )
 	{
 		if (d_len_empty != 1)
-			ft_strlcpy (d++, s, d_len_empty--);
-	s++;
+			ft_strlcpy (tmpdest++, tmpsrc, d_len_empty--);
+	tmpsrc++;
 	}
-	*d = '\0';
-	return (d_len + (s - src));
+	*tmpdest = '\0';
+	return (d_len + (tmpsrc - src));
 }

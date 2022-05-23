dfc95487b09d275d0a60bcc15346084043d17d2d
Mohammad abir Abbas <mabbas@c2s13d70.42wolfsburg.de>
2022-05-23 14:39:03 +0200

    stupid version

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,33 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mabbas <mabbas@student.wolfsburg42.de>     +#+  +:+       +#+        */
+/*   By: mabbas <mabbas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/19 21:13:25 by mabbas            #+#    #+#             */
-/*   Updated: 2022/05/20 00:49:14 by mabbas           ###   ########.fr       */
+/*   Created: 2022/05/22 23:29:07 by mabbas            #+#    #+#             */
+/*   Updated: 2022/05/23 14:35:43 by mabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *str, char delimiters)
+static int  ft_sen_len(char *str,char d)
 {
-	char	*aux_string;
-	char	*tmp;
-	size_t	count;
+    int i;
+    int wrd_c;
 
-	aux_string = (char *)malloc(sizeof(char) * ft_strlen(s));
-	aux_string = ft_strdup(s);
-	tmp = aux_string;
-	count = 0;
-	while (*tmp != '\0')
-	{
-		if (*tmp == c)
-			count++;
-		tmp++;
-	}
-	*tmp = NULL;
-	while(!)
+    i = 0;
+    wrd_c = 0;
+    while(str[i])
+    {
+        while(str[i] == d)
+            i++;
+        if(str[i])
+        {
+            while(str[i] && str[i] != d)
+                i++;
+            sen_c++;
+        }
+    }
+    return (sen_c);
+}
 
+static int ft_arr_fill(char *str, int i, char *d)
+{
+    int c;
+    c = 0;
+    while(str[i] && str[i] != d)
+    {
+        i++;
+        c++;
+    }
+}
 
+char    **ft_split(char *str, char d)
+{
+    char **result_arr;
+
+    result_arr = (char **)malloc(sizeof(char *) * ft_sen_len(str,d) + 1));
+    if (!result_arr)
+        return (NULL);
+    ft_fill_array(result_arr,str,d)
+    return (result_arr);
+}
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("%d", num_words("    abir   is   here, sd,ad dsadas a das ",','));
+    printf("%s", ft_substr("this is a sentence",0,ft_strlen(s)));
 }

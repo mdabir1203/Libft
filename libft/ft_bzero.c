f634a8539dca6f3300e64409922f7a578c08b858
Mohammad abir Abbas <mabbas@c2s19d129.42wolfsburg.de>
2022-06-04 15:06:30 +0200

    bulletproof now

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,14 +6,13 @@
 /*   By: mabbas <mabbas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 18:29:51 by mabbas            #+#    #+#             */
-/*   Updated: 2022/06/04 13:41:27 by mabbas           ###   ########.fr       */
+/*   Updated: 2022/06/04 14:50:44 by mabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
-
 {
 	ft_memset (s, '\0', n);
 }
